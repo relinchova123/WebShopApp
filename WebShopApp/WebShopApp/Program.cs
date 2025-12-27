be044aff8d0da30b26e8efa65306123ada992281
relinchova123 <relinchova@pmg-pernik.com>
2025-12-27 12:12:44 +0200

    Part-2

--- WebShopApp/WebShopApp/Program.cs
+++ WebShopApp/WebShopApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebShopApp.Infrastructure.Data;
+using WebShopApp.Infrastructure.Data.Entities;
 
 namespace WebShopApp
 {
@@ -16,7 +17,7 @@ namespace WebShopApp
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => {
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => {
                 options.SignIn.RequireConfirmedAccount = false;
                 options.Password.RequireDigit = false;
                 options.Password.RequireLowercase = false;
