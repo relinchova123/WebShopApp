ef95f4b75c2740e7dc968104af14f4394c70bb68
relinchova123 <relinchova@pmg-pernik.com>
2025-12-29 07:14:53 +0200

    Part-4

---
+++ WebShopApp/WebShopApp/Views/Product/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebShopApp.Views.Product
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebShopApp/WebShopApp/Views/Product/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebShopApp.Views.Product
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
