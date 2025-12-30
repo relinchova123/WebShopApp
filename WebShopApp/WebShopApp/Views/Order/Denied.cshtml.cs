02dddc403911bbb51e35ad09ec7dedf8cbe5a002
relinchova123 <relinchova@pmg-pernik.com>
2025-12-30 08:35:27 +0200

    Part-5

---
+++ WebShopApp/WebShopApp/Views/Order/Denied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebShopApp.Views.Order
+{
+    public class DeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
