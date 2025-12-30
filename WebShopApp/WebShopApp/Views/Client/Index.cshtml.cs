88ed658658a780be5224d461e9b971c49a84915d
relinchova123 <relinchova@pmg-pernik.com>
2025-12-30 09:07:18 +0200

    Part-6

---
+++ WebShopApp/WebShopApp/Views/Client/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebShopApp.Views.Client
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebShopApp/WebShopApp/Views/Client/NoDelete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebShopApp.Views.Client
+{
+    public class NoDeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
